9380703efedbf37ea5c81242ef923d4c4e920cb5
scaleupVinay <vinayaka@scaleupapp@club>
2024-12-17 22:58:42 +0530

    textinput ref

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {forwardRef, useState} from 'react';
 import {
   TextInput,
   View,
@@ -11,20 +11,23 @@ import {nh, nw} from '../helper/scales';
 import {COLORS} from '../helper/colors';
 import {APP_FONTS} from '../assets/fonts';
 
-const CustomTextInput = ({
-  placeholder = 'Enter text', // Placeholder text
-  errorMessage = '', // Error message (icon + text)
-  successMessage = '', // Success message (icon + text)
-  rightIcon, // Component for the right-side icon
-  onRightIconPress = () => {}, // Callback for right icon press
-  marginBottom = 15,
-  dropDown = false,
-  label = '',
-  textinputType = '',
-  width = '',
-  height = 40,
-  ...props
-}) => {
+const CustomTextInput = (
+  {
+    placeholder = 'Enter text', // Placeholder text
+    errorMessage = '', // Error message (icon + text)
+    successMessage = '', // Success message (icon + text)
+    rightIcon, // Component for the right-side icon
+    onRightIconPress = () => {}, // Callback for right icon press
+    marginBottom = 15,
+    dropDown = false,
+    label = '',
+    textinputType = '',
+    width = '',
+    height = 40,
+    ...props
+  },
+  ref,
+) => {
   const [selectedCountry, setSelectedCountry] = useState({
     name: 'India',
     flag: '🇮🇳',
@@ -68,6 +71,7 @@ const CustomTextInput = ({
           </TouchableOpacity>
         )}
         <TextInput
+          ref={ref}
           style={[
             styles.input,
             {
@@ -158,4 +162,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CustomTextInput;
+export default forwardRef(CustomTextInput);
