3e89467b845a9614ae5aea498f48c855e2a714c0
scaleupVinay <vinayaka@scaleupapp@club>
2025-01-02 22:02:11 +0530

    myprofile on click post changes

--- src/screens/Post/UserPost.js
+++ src/screens/Post/UserPost.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   SafeAreaView,
@@ -12,28 +12,8 @@ import PostView from '../Home/Post';
 import Header from '../../components/Header';
 
 const UserPost = ({route}) => {
-  const {index, data} = route?.params;
+  const {data} = route?.params;
   const [isPlaying, setIsPlaying] = useState(null);
-  const flatListRef = useRef(null);
-
-  useEffect(() => {
-    // Automatically scroll to the selected index
-    setTimeout(() => {
-      flatListRef.current?.scrollToIndex({
-        index: index ?? 0,
-        animated: true,
-      });
-    }, 500);
-  }, [index]);
-
-  const handleScrollToIndexFailed = info => {
-    setTimeout(() => {
-      flatListRef.current?.scrollToIndex({
-        index: index ?? 0,
-        animated: true,
-      });
-    }, 500);
-  };
 
   const renderItem = ({item, index}) => {
     return (
@@ -57,13 +37,10 @@ const UserPost = ({route}) => {
       <View style={styles.layer1}>
         <View style={styles.layer2}>
           <FlatList
-            ref={flatListRef}
             data={data?.content}
             keyExtractor={(_, index) => index.toString()}
             showsVerticalScrollIndicator={false}
             renderItem={renderItem}
-            onScrollToIndexFailed={handleScrollToIndexFailed}
-            initialNumToRender={50}
           />
         </View>
       </View>
