6504301af2f99f67281dc0c767b4f3171cd01c8b
scaleupVinay <vinayaka@scaleupapp@club>
2025-01-09 10:02:19 +0530

    api implemented

--- src/services/apiConstent.ts
+++ src/services/apiConstent.ts
@@ -53,8 +53,9 @@ export const API = {
   NOTIFICATION: 'content/notifications',
   INNERCIRCLE: 'users/inner-circle/requests',
   ACCEPTREQ: 'users/inner-circle/handle-request',
-  WIDRAW:'users/inner-circle/withdraw',
+  WIDRAW: 'users/inner-circle/withdraw',
   REMOVEREQ: 'users/inner-circle/remove',
   GETINNERCIRCLE: 'users/inner-circle',
- SENDINNERREQUEST :'users/inner-circle/request',
+  SENDINNERREQUEST: 'users/inner-circle/request',
+  USERANALYTICS: 'users/user-analytics',
 };
